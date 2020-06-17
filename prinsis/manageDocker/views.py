8957a3accb4d58e7c920e4e05e3da82403b71358
TristaRin <s105213018@mail1.ncnu.edu.tw>
2020-06-17 22:27:16 +0800

    modify app's api

--- prinsis/manageDocker/views.py
+++ prinsis/manageDocker/views.py
@@ -8,30 +8,28 @@ def manage(request) :
     ctx ={}
     if request.POST:
         # 拿到計算所需的參數
-        test = request.POST['test']
+        image = request.POST['image']
+        command = request.POST['command']
+        name = request.POST['name']
+
+
         # modelsStr  = request.POST['路徑模式'].split(',')
-
         # n = int(request.POST['初代數量'])
-        # growthCap = int(request.POST['族群最大數量'])
-        # lowerLimit = int(request.POST['族群最小數量'])
-        # iterationsNum = int(request.POST['演化次數'])
-        # mutCap = int(request.POST['突變最多次數'])
-
         # goodDNA, stepList, qualityList, spendTime = ga.myGA(unitsStr, modelsStr, n, growthCap, iterationsNum, lowerLimit, mutCap)
 
-        ctx['test'] = test
+        ctx['image'] = image
+        ctx['command'] = command
+        ctx['name'] = name
+
+        ctx['id'] = 'ididid'
+        ctx['status'] = 'stststst'
+
+
         # ctx['lowerLimit'] = lowerLimit
-        # ctx['iterationsNum'] = iterationsNum
-        # ctx['mutCap'] = mutCap
 
-
-        # ctx['goodDNA'] = goodDNA
         # ctx['goodStep'] = min(stepList)
         # ctx['goodQuality'] = max(qualityList)
-        # ctx['spendTime'] = spendTime
         # stepList = map(str, stepList)
-        # qualityList = map(str, qualityList)
-        # ctx['stepList'] = str(','.join(stepList))
         # ctx['qualityList'] = str(','.join(qualityList))
 
     # Repeat a few rounds
