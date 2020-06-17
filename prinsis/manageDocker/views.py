968101b9b8524d912a1f574a907b23a2dece71bf
sister <lucyou871029@gmail.com>
2020-06-18 00:28:40 +0800

    search name and pull image

--- prinsis/manageDocker/views.py
+++ prinsis/manageDocker/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 import os
+import json
+import subprocess
 def hello_world(request):
     response = render(request, 'hello_world.html',{})
     return response
@@ -12,25 +14,37 @@ def manage(request) :
         image = request.POST['image']
         command = request.POST['command']
         name = request.POST['name']
-        
-        build_cmd = "curl --unix-socket /var/run/docker.sock -H"+' "Content-Type: application/json"'+" -d '{"+'"Image"'+':"'+image+'",'+' "Cmd":"'+ command+'"'+"}' -X POST http:/v1.24/containers/create"
+       
 
+        check_images = False
+
+        images_json = subprocess.Popen('curl --unix-socket /var/run/docker.sock http:/v1.24/images/json').stdout
+        images_list = json.loads(images_json.strip())
+        for each_image_dict in images_list:
+            if image in each_image_dict.['RepoTags']:
+                check_images = True
+        if check_images:
+            os.system('docker pull '+image)
+
+        build_cmd = "curl --unix-socket /var/run/docker.sock -H"+' "Content-Type: application/json"'+" -d '{"+'"Image"'+':"'+image+'",'+' "Cmd":"'+ command+'"'+"}' -X POST http:/v1.24/containers/create"
+        
         tmp = os.popen(build_cmd).readlines()[0]
         tmp_deal = eval(tmp.strip())
         tmp_dict = {}
         tmp_dict = tmp_deal
         print(tmp_dict)
+        
+        id_cmd = tmp_dict['Id'][0:12]
+        search = 'docker ps -a | grep "'+ id_cmd +'"'
 
-        # modelsStr  = request.POST['路徑模式'].split(',')
-        # n = int(request.POST['初代數量'])
-        # goodDNA, stepList, qualityList, spendTime = ga.myGA(unitsStr, modelsStr, n, growthCap, iterationsNum, lowerLimit, mutCap)
+        search_name = search.split()[-1]
 
         ctx['image'] = image
         ctx['command'] = command
         ctx['name'] = name
 
-        ctx['id'] = tmp_dict['Id']
-        ctx['status'] = tmp_dict['Warnings']
+        ctx['id'] = tmp_dict['Id'][0:12]
+        ctx['status'] = search_name
 
 
         # ctx['lowerLimit'] = lowerLimit
